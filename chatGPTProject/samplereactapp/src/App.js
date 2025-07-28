b7021992801b8f2eaf6220d89d1ad517182ecd7a
gautamsa <satyamsinghgautam@gmail.com>
2025-07-28 10:37:53 +0530

    Changes

--- chatGPTProject/samplereactapp/src/App.js
+++ chatGPTProject/samplereactapp/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div>Hello World!</div>
+        <div>Hello Satyam!</div>
       </header>
     </div>
   );
